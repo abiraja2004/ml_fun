6f0658e75aef2de4e34caa2071f656abc6fc6234
Kenneth Cavagnolo <kcavagnolo@users.noreply.github.com>
2016-12-27 20:31:19 -0500

    Moved docker out

--- docker_ml/dsci_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-/usr/lib/rstudio-server/bin/rserver --server-daemonize 0 &
-/usr/bin/shiny-server &
-java -jar /opt/h2o.jar &
-jupyter notebook --ip=0.0.0.0 --no-browser &
